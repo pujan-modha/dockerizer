ac17c87aa6be3ce09ece13f5d3bd990d399b213c
pujan-modha <pujanmodha19@gmail.com>
2025-05-03 18:09:23 +0530

    update packages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   distDir: "dist",
-  basePath: process.env.NODE_ENV === "production" ? "/dockerizer" : undefined,
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   images: { unoptimized: true },
   reactStrictMode: true,
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
+export default nextConfig;
